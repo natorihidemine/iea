80ab96a8871bc5e4a5b6d0637fcbb00c1bfa0b00
tech-camp <>
2019-06-13 16:54:31 +0900

    とりあえずマップ表示

--- iearuko/resources/views/rooms/show.blade.php
+++ iearuko/resources/views/rooms/show.blade.php
@@ -12,4 +12,22 @@
 
 <h2>土地勘MAP 月島荘</h2>
 
+<div class=tochikanmap>
+  <!-- canvas -->
+  <div id="map-canvas"></div>
+
+  <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ"></script>
+  <script>
+var mapDiv = document.getElementById( "map-canvas" ) ;
+
+var map = new google.maps.Map( mapDiv, {
+  center: new google.maps.LatLng( 35.7100, 139.8107 ) ,
+  zoom: 11 ,
+} ) ;
+  </script>
+
+<div> <input type="checkbox" >コンビニ&nbsp;&nbsp;<input type="checkbox" >スーパー</div>
+
+</div>
+
 @endsection('content')
