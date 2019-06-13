1a83c2a6d83a766e4dd9dff8618f64396bd91744
tech-camp <>
2019-06-13 17:19:34 +0900

    checkboxの編集

--- iearuko/resources/views/rooms/index.blade.php
+++ iearuko/resources/views/rooms/index.blade.php
@@ -16,43 +16,43 @@
 
 <div class="kensaku">
   <div> 賃料&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;：&nbsp;&nbsp;下限
-  <select id="language" name="language">
-<option value="en" selected>なし</option>
-<option value="ja" >3万円</option>
-<option value="zh"> 4万円</option>
-<option value="zh"> 5万円</option>
-<option value="zh"> 6万円</option>
-<option value="zh"> 7万円</option>
-<option value="zh"> 8万円</option>
-<option value="zh"> 9万円</option>
-<option value="zh"> 10万円</option>
-<option value="zh"> 12.5万円</option>
-<option value="zh"> 15万円</option>
-<option value="zh"> 17.5万円</option>
-<option value="zh"> 20万円</option>
-<option value="zh"> 25万円</option>
-<option value="zh"> 30万円</option>
-<option value="zh"> 40万円</option>
-<option value="zh"> 50万円</option>
+  <select id="price" name="min">
+<option value="なし" selected>なし</option>
+<option value="3" >3万円</option>
+<option value="4"> 4万円</option>
+<option value="5"> 5万円</option>
+<option value="6"> 6万円</option>
+<option value="7"> 7万円</option>
+<option value="8"> 8万円</option>
+<option value="9"> 9万円</option>
+<option value="10"> 10万円</option>
+<option value="12.5"> 12.5万円</option>
+<option value="15"> 15万円</option>
+<option value="17.5"> 17.5万円</option>
+<option value="20"> 20万円</option>
+<option value="25"> 25万円</option>
+<option value="30"> 30万円</option>
+<option value="40"> 40万円</option>
+<option value="50"> 50万円</option>
 </select>
-〜上限<select id="language" name="language">
-<option value="en" selected>なし</option>
-<option value="ja" >3万円</option>
-<option value="zh"> 4万円</option>
-<option value="zh"> 5万円</option>
-<option value="zh"> 6万円</option>
-<option value="zh"> 7万円</option>
-<option value="zh"> 8万円</option>
-<option value="zh"> 9万円</option>
-<option value="zh"> 10万円</option>
-<option value="zh"> 12.5万円</option>
-<option value="zh"> 15万円</option>
-<option value="zh"> 17.5万円</option>
-<option value="zh"> 20万円</option>
-<option value="zh"> 25万円</option>
-<option value="zh"> 30万円</option>
-<option value="zh"> 40万円</option>
-<option value="zh"> 50万円</option>
+〜上限<select id="price" name="max">
+<option value="なし" selected>なし</option>
+<option value="3" >3万円</option>
+<option value="4"> 4万円</option>
+<option value="5"> 5万円</option>
+<option value="6"> 6万円</option>
+<option value="7"> 7万円</option>
+<option value="8"> 8万円</option>
+<option value="9"> 9万円</option>
+<option value="10"> 10万円</option>
+<option value="12.5"> 12.5万円</option>
+<option value="15"> 15万円</option>
+<option value="17.5"> 17.5万円</option>
+<option value="20"> 20万円</option>
+<option value="25"> 25万円</option>
+<option value="30"> 30万円</option>
+<option value="40"> 40万円</option>
+<option value="50"> 50万円</option>
 </select>
 </div>
   <div> 間取り&nbsp;&nbsp;&nbsp;：&nbsp;&nbsp;<input type="checkbox" >ワンルーム&nbsp;&nbsp;<input type="checkbox" >1LDK&nbsp;&nbsp;<input type="checkbox" >1DK&nbsp;&nbsp;<input type="checkbox" >1LDK<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" >2K&nbsp;&nbsp;<input type="checkbox" >2DK&nbsp;&nbsp;<input type="checkbox" >2LDK<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" >3K&nbsp;&nbsp;<input type="checkbox" >3DK&nbsp;&nbsp;<input type="checkbox" >3LDK&nbsp;&nbsp;<input type="checkbox" >4K以上</div>
