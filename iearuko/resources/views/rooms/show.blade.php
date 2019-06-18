acce01f1dcb461d368e3764369f44247b9a4bdc8
tech-camp <>
2019-06-18 10:44:29 +0900

    コンビニにマーカーたてたよ

--- iearuko/resources/views/rooms/show.blade.php
+++ iearuko/resources/views/rooms/show.blade.php
@@ -14,10 +14,15 @@
 
 <div class=tochikanmap>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ&libraries=places" type="text/javascript"></script>
 <script type="text/javascript">
     //COMTOPIA流Google MAP表示方法
     var geocoder = new google.maps.Geocoder();//Geocode APIを使います。
+    var service;
+    var infowindow;
+
+
     var address = "{{$round->address}}";
     geocoder.geocode({'address': address,'language':'ja'},function(results, status){
         if (status == google.maps.GeocoderStatus.OK){
@@ -33,10 +38,34 @@
                 position: map.getCenter(),
                 map: map
             });
+
+            var request = {
+          location: latlng,
+          radius: '1500',
+          type: ['convenience_store']　 // https://developers.google.com/places/supported_types 参照
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+
+        function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            latlng = place.geometry.location;
+            icn = place.icon;
+            var marker = new google.maps.Marker({//住所のポイントにマーカーを立てる
+                position: latlng,
+                map: map
+            });
+          }
+        }
+      }
+
         }else{
             alert("Geocode was not successful for the following reason: " + status);
         }
     });
+
 </script>
 <div id="google_map" style="width:70%;height:500px"></div>
 
