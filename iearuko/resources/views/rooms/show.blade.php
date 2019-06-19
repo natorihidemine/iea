4ac2772c13f1fd5c5cbce9021b6b660f3a777918
tech-camp <>
2019-06-19 14:18:41 +0900

    検索ボックスの追加

--- iearuko/resources/views/rooms/show.blade.php
+++ iearuko/resources/views/rooms/show.blade.php
@@ -11,11 +11,11 @@
 <section>
 
 <h2>土地勘MAP {{$round->name}}</h2>
-
+<input id="pac-input" class="controls" type="text" placeholder="フリーワード">
 <div class=tochikanmap>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ&libraries=places" type="text/javascript"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
 <script type="text/javascript">
     //COMTOPIA流Google MAP表示方法
     var geocoder = new google.maps.Geocoder();//Geocode APIを使います。
@@ -29,7 +29,7 @@
             var latlng=results[0].geometry.location;//緯度と経度を取得
             var mapOpt = {
                 center: latlng,//取得した緯度経度を地図の真ん中に設定
-                zoom: 15,//地図倍率1～20
+                zoom: 16,//地図倍率1～20
                 mapTypeId: google.maps.MapTypeId.ROADMAP//普通の道路マップ
 
             };
@@ -41,9 +41,67 @@
           };
             var marker = new google.maps.Marker(markerOptions);
 
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+
             var request = {
           location: latlng,
-          radius: '1500',
+          radius: '1000',
           type: ['convenience_store'] // https://developers.google.com/places/supported_types 参照
 
         };
@@ -58,8 +116,8 @@
           map: map,
         });
         var placename = place.name;
-// 吹き出しにカフェの名前を埋め込む
-      var contentString = `<div class="sample"><p id="place_name">${placename}</p></div>`;
+        var contentString = `<div class="sample"><p id="place_name">${placename}</p></div>`;
+
 
      // 吹き出し
       var infoWindow = new google.maps.InfoWindow({ // 吹き出しの追加
@@ -71,6 +129,7 @@
       infoWindow.open(map, marker_around); // 吹き出しの表示
     });
       }
+
 
         function callback(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
