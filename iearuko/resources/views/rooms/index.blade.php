bb29da6faf55cd231533c82b615a421dbf39e054
tech-camp <>
2019-07-01 11:10:22 +0900

    no message

--- iearuko/resources/views/rooms/index.blade.php
+++ iearuko/resources/views/rooms/index.blade.php
@@ -110,7 +110,7 @@
 
 </form>
 
-<script src="http://code.jquery.com/jquery-1.12.3.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript">
             var stat = document.getElementsByName('stat[]');
             $(stat).click(function() {
